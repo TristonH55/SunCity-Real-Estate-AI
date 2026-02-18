4b377fea656c2b8095ddd227016ee525cb6af147
TristonH55 <oop@oopdesign.com.au>
2026-02-18 14:21:05 +1000

    LAST UPDATE/ CHANGED DB

--- app/api/admin/admin/reject/route.ts
+++ app/api/admin/admin/reject/route.ts
@@ -1,3 +1,7 @@
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
+
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { verifyApprovalToken } from "@/lib/approval-token";
