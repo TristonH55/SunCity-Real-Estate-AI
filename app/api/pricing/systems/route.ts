52deca32a4557593bcf13deba4c8f9a46e0a0fe2
TristonH55 <oop@oopdesign.com.au>
2026-02-18 14:55:00 +1000

    LAST UPDATE/ CHANGED DB 5

--- app/api/pricing/systems/route.ts
+++ app/api/pricing/systems/route.ts
@@ -62,6 +62,9 @@
 // }
 /////////////
 
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
 import { NextResponse } from "next/server";
 import { prisma } from "lib/prisma";
 
